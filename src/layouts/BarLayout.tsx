0766f3ca6b15ebdca1467bbd93043015dbaef3a2
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-06-15 12:38:19 +0530

    add media query for height

--- src/layouts/BarLayout.tsx
+++ src/layouts/BarLayout.tsx
@@ -5,7 +5,7 @@ import ModalLayout from "./ModalLayout.tsx";
 function BarLayout() {
 	return (
 		<>
-			<div className="flex h-3/4 w-200 flex-col items-center justify-end gap-4 overflow-hidden rounded-md border border-gray-300 bg-gray-50 p-4">
+			<div className="bar__container flex w-200 flex-col items-center justify-end gap-4 overflow-hidden rounded-md border border-gray-300 bg-gray-50 p-4">
 				<BarContainer />
 
 				<p className="h-0.5 w-full bg-gray-400"></p>
